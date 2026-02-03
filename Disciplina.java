58a72e7e867693841798b10a365dd41fd2ee2174
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:29:43 -0300

    Update Disciplina.java

--- Disciplina.java
+++ Disciplina.java
@@ -1,5 +1,45 @@
 package com.sistemaEscola.entities;
 
-public class Disciplina {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "disciplina")
+public class Disciplina {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private double cargaHoraria;
+	private String descricao;
+	
+	public Disciplina () {
+		
+	}
+	
+	public Disciplina (String nome, double cargaHoraria, String descricao) {
+		this.nome = nome;
+		this.cargaHoraria = cargaHoraria;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getCargaHoraria() {
+		return cargaHoraria;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
 }
+
