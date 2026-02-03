03f7ece3f1aa08396efff5b24c884e6550e0c6b1
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:30:49 -0300

    Update Matricula.java

--- Matricula.java
+++ Matricula.java
@@ -1,5 +1,42 @@
 package com.sistemaEscola.entities;
 
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "matricula")
 public class Matricula {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate dataMatricula;
+	private String status;
+	
+	public Matricula () {
+		
+	}
+	
+	public Matricula (LocalDate dataMatricula, String status) {
+		this.dataMatricula = dataMatricula;
+		this.status = status;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public LocalDate getDataMatricula() {
+		return dataMatricula;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
 
 }
+
