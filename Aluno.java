07a8fec52a8650b5adc8ab93158fe8c2c11b5e79
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:28:29 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,5 +1,47 @@
 package com.sistemaEscola.entities;
 
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
 public class Aluno {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private LocalDate dataNascimento;
+	
+	public Aluno () {
+		
+	}
+	
+	public Aluno (String nome, String email, LocalDate dataNascimento) {
+		this.nome = nome;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	
 
 }
