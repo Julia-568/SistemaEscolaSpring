6448a19e7e7d873fd4d75652b5a942cffa448b41
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:29:05 -0300

    Add Curso entity with JPA annotations

--- Curso.java
+++ Curso.java
@@ -1,5 +1,40 @@
 package com.sistemaEscola.entities;
 
-public class Curso {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "curso")
+public class Curso {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private double cargaHoraria;
+	private String descricao;
+	
+	public Curso () {
+		
+	}
+	
+	public Curso (double cargaHoraria, String descricao) {
+		this.cargaHoraria = cargaHoraria;
+		this.descricao = descricao;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public double getCargaHoraria() {
+		return cargaHoraria;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
 }
