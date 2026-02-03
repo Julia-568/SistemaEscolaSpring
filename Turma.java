7a09c7dd5ebc2f6ae7b44046cbd2f8aabe2fa438
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:31:56 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -1,5 +1,45 @@
 package com.sistemaEscola.entities;
 
-public class Turma {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "turma")
+public class Turma {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private int codigoTurma;
+	private int ano;
+	private String periodo;
+	
+	public Turma () {
+		
+	}
+	
+	public Turma (int codigoTurma, int ano, String periodo) {
+		this.codigoTurma = codigoTurma;
+		this.ano = ano;
+		this.periodo = periodo;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public int getCodigoTurma() {
+		return codigoTurma;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public String getPeriodo() {
+		return periodo;
+	}
+	
 }
